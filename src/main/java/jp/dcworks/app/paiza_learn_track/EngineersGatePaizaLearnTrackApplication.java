acecbf05cf67d0bd9d65922cb9e9889eb96c3116
Tomomichi Sato <tsato@dcworks.jp>
2023-06-09 15:06:51 +0900

    EngineersGate 受講生進捗管理のバッチ作成。

---
+++ src/main/java/jp/dcworks/app/paiza_learn_track/EngineersGatePaizaLearnTrackApplication.java
@@ -0,0 +1,13 @@
+package jp.dcworks.app.paiza_learn_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineersGatePaizaLearnTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineersGatePaizaLearnTrackApplication.class, args);
+	}
+
+}
