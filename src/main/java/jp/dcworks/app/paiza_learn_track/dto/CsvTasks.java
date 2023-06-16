ab35e232059db261c29a02ea76fe299e7770a215
Tomomichi Sato <tsato@dcworks.jp>
2023-06-16 15:07:55 +0900

    EngineersGate 受講生進捗管理のバッチ作成。 課題CSV用DTO追加。

---
+++ src/main/java/jp/dcworks/app/paiza_learn_track/dto/CsvTasks.java
@@ -0,0 +1,38 @@
+package jp.dcworks.app.paiza_learn_track.dto;
+
+import lombok.Data;
+
+/**
+ * CSVファイルから変換した課題データクラス。
+ *
+ * @author tomo-sato
+ */
+@Data
+public class CsvTasks {
+	/** 講座ID */
+	private String courseId;
+
+	/** 講座名 */
+	private String courseName;
+
+	/** レッスンID */
+	private String lessonId;
+
+	/** レッスン名 */
+	private String lessonName;
+
+	/** チャプターID */
+	private String chapterId;
+
+	/** チャプター名 */
+	private String chapterName;
+
+	/** 演習課題数（※csvファイルに記載がないので、目でpaizaのサイトを見て手動メンテ） */
+	private String exerciseNum;
+
+	/** 課題種別ID（※1.paiza、2.オリジナル課題） */
+	private String taskTypesId;
+
+	/** 学習時間（時）：算出した学習時間及び、割り当てた学習時間を登録（整数4桁、少数2桁） */
+	private String learningHours;
+}
