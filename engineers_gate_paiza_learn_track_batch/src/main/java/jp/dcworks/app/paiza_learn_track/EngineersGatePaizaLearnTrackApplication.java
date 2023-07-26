0682d8dcfe67cd88eceddf31915d9d9f14cc993f
Tomomichi Sato <tsato@dcworks.jp>
2023-07-26 17:26:50 +0900

    EngineersGate 受講生進捗管理アプリ。 ukに引っかかっていたのでSQL修正。

--- engineers_gate_paiza_learn_track_batch/src/main/java/jp/dcworks/app/paiza_learn_track/EngineersGatePaizaLearnTrackApplication.java
+++ engineers_gate_paiza_learn_track_batch/src/main/java/jp/dcworks/app/paiza_learn_track/EngineersGatePaizaLearnTrackApplication.java
@@ -2,8 +2,12 @@ package jp.dcworks.app.paiza_learn_track;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("jp.dcworks.app.paiza_learn_track_library.entity")
+@EnableJpaRepositories("jp.dcworks.app.paiza_learn_track_library.repository")
 public class EngineersGatePaizaLearnTrackApplication {
 
 	public static void main(String[] args) {
--- engineers_gate_paiza_learn_track_web/src/main/java/jp/dcworks/app/paiza_learn_track_web/EngineersGatePaizaLearnTrackWebApplication.java
+++ engineers_gate_paiza_learn_track_web/src/main/java/jp/dcworks/app/paiza_learn_track_web/EngineersGatePaizaLearnTrackWebApplication.java
@@ -3,9 +3,11 @@ package jp.dcworks.app.paiza_learn_track_web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("jp.dcworks.app.paiza_learn_track_library.entity")
+@EnableJpaRepositories("jp.dcworks.app.paiza_learn_track_library.repository")
 public class EngineersGatePaizaLearnTrackWebApplication {
 
 	public static void main(String[] args) {
